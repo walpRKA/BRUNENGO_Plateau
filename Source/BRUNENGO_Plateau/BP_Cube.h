77332b14569183503ac6d80d49a84ab0f35e72d0
walpRKA <145676508+walpRKA@users.noreply.github.com>
2024-10-01 17:01:00 +0200

    push cube rouge+c++

---
+++ Source/BRUNENGO_Plateau/BP_Cube.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BP_Cube.generated.h"
+
+UCLASS(Blueprintable)
+class BRUNENGO_PLATEAU_API ABP_Cube : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABP_Cube();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
